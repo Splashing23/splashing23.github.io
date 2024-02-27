6adeef9a1a3f3c3eae16960bd64bb28aa898cc50
= <=>
2024-02-27 11:28:13 -0600

    redraw thumbnails

--- make_thumbnails.sh
+++ make_thumbnails.sh
@@ -1,5 +1,6 @@
-for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file" -resize 160x160 "tn/$file"
+for file in images/*.{jpg,jpeg,png}; do
+    #[ ! -f "tn/$file" ] && 
+    convert "$file" -resize 250x250 "tn/$file"
 done
 
-convert images/portrait_circle.jpg -resize 2000x2000 "tn/images/portrait_circle.jpg"
+convert images/portrait_circle.jpg -resize 5000x5000 "tn/images/portrait_circle.jpg"
