b753cb692e3949b14473fb3636ac390815350558
= <=>
2024-02-27 11:25:14 -0600

    update image

--- make_thumbnails.sh
+++ make_thumbnails.sh
@@ -2,4 +2,4 @@ for file in images/*.{jpg,png}; do
     [ ! -f "tn/$file" ] && convert "$file" -resize 160x160 "tn/$file"
 done
 
-convert images/portrait_circle.jpg -resize 500x500 "tn/images/portrait_circle.jpg"
+convert images/portrait_circle.jpg -resize 2000x2000 "tn/images/portrait_circle.jpg"
