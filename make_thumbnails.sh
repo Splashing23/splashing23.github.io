95aeafd219cb8eba371f29ef6d5a6f28dd5e7d7f
= <=>
2024-02-27 13:17:42 -0600

    make images smaller

--- make_thumbnails.sh
+++ make_thumbnails.sh
@@ -1,6 +1,6 @@
 for file in images/*.{jpg,jpeg,png}; do
     #[ ! -f "tn/$file" ] && 
-    convert "$file" -thumbnail 400x400 "tn/$file"
+    convert "$file" -thumbnail 275 "tn/$file"
 done
 
 convert images/portrait_circle.jpg -resize 5000x5000 "tn/images/portrait_circle.jpg"
